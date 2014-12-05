2b502b79b724a07eb0d5ff8d413e2de074c3ee8d
Ketan Verma <ketan9495@gmail.com>
2014-12-05 22:14:54 +0530

    Added response docs for GeoIP class

--- Models/_GeoIP.php
+++ Models/_GeoIP.php
@@ -2,6 +2,27 @@
 
 	class _GeoIP {
 
+		/*
+			Response Data from this API (telize.com/geoip/<optional:ip>)
+
+			ip (Visitor IP address, or IP address specified as parameter)
+			country_code (Two-letter ISO 3166-1 alpha-2 country code)
+			country_code3 (Three-letter ISO 3166-1 alpha-3 country code)
+			country (Name of the country)
+			region_code (Two-letter ISO-3166-2 state / region code)
+			region (Name of the region)
+			city (Name of the city)
+			postal_code (Postal code / Zip code)
+			continent_code (Two-letter continent code)
+			latitude (Latitude)
+			longitude (Longitude)
+			dma_code (DMA Code)
+			area_code (Area Code)
+			asn (Autonomous System Number)
+			isp (Internet service provider)
+			timezone (Time Zone)
+		*/
+
 		public function ipdata($arg) {
 			if (isset($arg[0])) {
 				$ip = $arg[0];
